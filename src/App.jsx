461e5d44d53513fc7382ced6725d87478cde1fc8
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2022-05-12 12:58:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
       <Route
-          path="/create"
+          path="/"
           element={<Blocks />}
         ></Route>
       
