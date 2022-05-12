e42f0b214fc3c8a3ab0e3ad6aae1ae564b2d9c25
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2022-05-12 13:46:59 +0530

    cm5

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -19,35 +19,89 @@ function Blocks() {
     document.getElementById("edited").value = "";
   }
 
+  function edit1() {
+    document.getElementById("old1").style.display = "none";
+    document.getElementById("new1").style.display = "block";
+  }
+
+  function required1() {
+    let data = document.getElementById("label1");
+    data.innerHTML = document.getElementById("edited1").value;
+
+    document.getElementById("old1").style.display = "block";
+    document.getElementById("new1").style.display = "none";
+
+    document.getElementById("edited1").value = "";
+  }
+
+  function edit2() {
+    document.getElementById("old2").style.display = "none";
+    document.getElementById("new2").style.display = "block";
+  }
+
+  function required2() {
+    let data = document.getElementById("label2");
+    data.innerHTML = document.getElementById("edited2").value;
+
+    document.getElementById("old2").style.display = "block";
+    document.getElementById("new2").style.display = "none";
+
+    document.getElementById("edited2").value = "";
+  }
+
+  function edit3() {
+    document.getElementById("old3").style.display = "none";
+    document.getElementById("new3").style.display = "block";
+  }
+
+  function required3() {
+    let data = document.getElementById("label3");
+    data.innerHTML = document.getElementById("edited3").value;
+
+    document.getElementById("old3").style.display = "block";
+    document.getElementById("new3").style.display = "none";
+
+    document.getElementById("edited3").value = "";
+  }
+
+  function edit4() {
+    document.getElementById("old4").style.display = "none";
+    document.getElementById("new4").style.display = "block";
+  }
+
+  function required4() {
+    let data = document.getElementById("label4");
+    data.innerHTML = document.getElementById("edited4").value;
+
+    document.getElementById("old4").style.display = "block";
+    document.getElementById("new4").style.display = "none";
+
+    document.getElementById("edited4").value = "";
+  }
+
   return (
     <div>
       <div className="flexbox">
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          marginLeft: "50px",
+        }}>
         <Form id="form1" className="form">
           <Input id="ip1" className="ip" draggable="true" clone="true">
-            <div id="old">
+            <div id="old" style={{
+              display:"flex",
+            }}>
               <p id="label">First Name</p>
-              <button
-                onClick={() => {
-                  edit();
-                }}
-              >
+              <button onClick={() => {edit();}}>
                 Edit
               </button>
               <br />
             </div>
 
-            <div
-              id="new"
-              style={{
-                display: "none",
-              }}
-            >
+            <div id="new" style={{display: "none",}}>
               <input type="text" id="edited" />
-              <button
-                onClick={() => {
-                  required();
-                }}
-              >
+              <button onClick={() => {required();}}>
                 OK
               </button>
             </div>
@@ -56,11 +110,93 @@ function Blocks() {
         </Form>
 
         <Form id="form2" className="form">
-          <Input id="ip2" className="ip" draggable="true">
-            <input type="text" placeholder="last name" />
-            <button>Edit</button>
+        <Input id="ip2" className="ip" draggable="true" clone="true">
+            <div id="old1" style={{
+              display:"flex",
+            }}>
+              <p id="label1">Last Name</p>
+              <button onClick={() => {edit1();}}>
+                Edit
+              </button>
+              <br />
+            </div>
+
+            <div id="new1" style={{display: "none",}}>
+              <input type="text" id="edited1" />
+              <button onClick={() => {required1();}}>
+                OK
+              </button>
+            </div>
+            <input type="text" placeholder="Enter Details" />
           </Input>
         </Form>
+
+        <Form id="form3" className="form">
+        <Input id="ip3" className="ip" draggable="true" clone="true">
+            <div id="old2" style={{
+              display:"flex",
+            }}>
+              <p id="label2">Email</p>
+              <button onClick={() => {edit2();}}>
+                Edit
+              </button>
+              <br />
+            </div>
+
+            <div id="new2" style={{display: "none",}}>
+              <input type="text" id="edited2" />
+              <button onClick={() => {required2();}}>
+                OK
+              </button>
+            </div>
+            <input type="email" placeholder="Enter Details" />
+          </Input>
+        </Form>
+
+        <Form id="form4" className="form">
+        <Input id="ip4" className="ip" draggable="true" clone="true">
+            <div id="old3" style={{
+              display:"flex",
+            }}>
+              <p id="label3">Phone Number</p>
+              <button onClick={() => {edit3();}}>
+                Edit
+              </button>
+              <br />
+            </div>
+
+            <div id="new3" style={{display: "none",}}>
+              <input type="text" id="edited3" />
+              <button onClick={() => {required3();}}>
+                OK
+              </button>
+            </div>
+            <input type="number" placeholder="Enter Details" />
+          </Input>
+        </Form>
+
+        <Form id="form5" className="form">
+        <Input id="ip5" className="ip" draggable="true" clone="true">
+            <div id="old4" style={{
+              display:"flex",
+            }}>
+              <p id="label4">Address</p>
+              <button onClick={() => {edit4();}}>
+                Edit
+              </button>
+              <br />
+            </div>
+
+            <div id="new4" style={{display: "none",}}>
+              <input type="text" id="edited4" />
+              <button onClick={() => {required4();}}>
+                OK
+              </button>
+            </div>
+            <input type="text" placeholder="Enter Details" />
+          </Input>
+        </Form>
+        </div>
 
         <hr />
         <div className="formfix">
